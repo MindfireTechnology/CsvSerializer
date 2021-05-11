52be12960d482510cc663f04293fc8c9124e68bb
Christian Jacob <cjacob@blueskyinnovations.de>
2021-05-11 21:42:47 +0200

    Added support for StringLengthAttribute

--- src/CsvSerializerTests/TestObjects.cs
+++ src/CsvSerializerTests/TestObjects.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using CsvSerializer;
@@ -22,6 +23,7 @@ namespace CsvSerializerTests
 		public string Id { get; set; }
 
 		[JsonProperty("first_name")]
+		[StringLength(7)]
 		public string FirstName { get; set; }
 
 		[JsonRequired]
